ac0f1ccb63d7a7f54d5798d59696a2686508a0e1
RoseSkullIXVI <athosjnr@gmail.com>
2024-11-05 21:23:44 +0200

    Add JWT authentication support with ConfigModule integration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,PassportModule , JwtModule.register({
+    secret: process.env.JWT_TOKEN,
+    signOptions : {expiresIn: parseInt(process.env.TOKEN_SEKUNDE)}
+  })],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor (private UsersService:UsersService) {}
+    constructor (private UsersService:UsersService,private jwtService: JwtService) {}
     async validateUser (username:string , pass:string): Promise<any>{
         const user = await this.UsersService.findOne(username);        
         const isMatch = await bcrypt.compare(pass, user.password);
@@ -15,6 +16,13 @@ export class AuthService {
         else{
             return null;
         }
+    }
+
+    async login (user:any){
+        const payload = {username: user.username , sub: user.userID};
+        return {
+            access_token : this.jwtService.sign(payload),
+        };
     }
 
 }
