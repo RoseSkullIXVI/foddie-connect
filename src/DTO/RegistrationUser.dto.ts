01b026dc6d8db5301c8c063a8a2b653e5a8edd89
RoseSkullIXVI <athosjnr@gmail.com>
2024-11-13 22:01:41 +0200

    Add class-validator for user registration and implement Credentials subscriber for email normalization and password hashing

---
+++ src/DTO/RegistrationUser.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword, MinLength } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsNotEmpty()
+  fullname: string;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
---
+++ src/Subscribers/Credentials.subscriber.ts
@@ -0,0 +1,22 @@
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent } from 'typeorm';
+import { Credentials } from '../Entities/credentials.entity';
+import * as bcrypt from 'bcrypt';
+
+@EventSubscriber()
+export class CredentialsSubscriber implements EntitySubscriberInterface<Credentials> {
+  listenTo() {
+    return Credentials;
+  }
+
+  async beforeInsert(event: InsertEvent<Credentials>) {
+    const { Email, password } = event.entity;
+
+    // Lowercase the email
+    event.entity.Email = Email.toLowerCase();
+
+    // Generate salt and hash the password
+    const salt = await bcrypt.genSalt();
+    event.entity.salt = salt;
+    event.entity.password = await bcrypt.hash(password, salt);
+  }
+}
